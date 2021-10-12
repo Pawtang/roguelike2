ff43a44fb80a33fd1922059d230ccb8539ef16e5
Ben Dibuz <ben.dibuz@gmail.com>
2021-10-11 22:28:26 -0500

    draggable

--- roguelike2/src/Inventory.js
+++ roguelike2/src/Inventory.js
@@ -4,18 +4,18 @@ const Inventory = () => {
            <div className="inventorycontainer">
                 <div className="equipped">
                     <div className="equippeditem placeholder"></div>
-                    <div className="equippeditem helmet" id="">Helmet<div class = 'sprite eq' id="helmet"></div></div>
+                    <div className="equippeditem helmet" id="">Helmet<div class = 'sprite eq' id="helmet" draggable="true"></div></div>
                     <div className="equippeditem placeholder"></div>
-                    <div className="equippeditem mainhand" id ="">Main Hand<div class = 'sprite eq' id="woodensword1"></div></div>
-                    <div className="equippeditem armor" id="">Armor<div class = 'sprite eq' id="cuirass"></div></div>
-                    <div className="equippeditem offhand" id="">Off Hand<div class = 'sprite eq' id="woodenshield"></div></div>
+                    <div className="equippeditem mainhand" id ="">Main Hand<div class = 'sprite eq' id="woodensword1" draggable="true"></div></div>
+                    <div className="equippeditem armor" id="">Armor<div class = 'sprite eq' id="cuirass" draggable="true"></div></div>
+                    <div className="equippeditem offhand" id="">Off Hand<div class = 'sprite eq' id="woodenshield" draggable="true"></div></div>
                     <div className="equippeditem gloves" id="">Gloves</div>
-                    <div className="equippeditem boots" id="">Boots<div class = 'sprite eq' id="boots"></div></div>
+                    <div className="equippeditem boots" id="">Boots<div class = 'sprite eq' id="boots" draggable="true"></div></div>
                     <div className="equippeditem amulet" id="">Amulet</div>
                 </div>
             <div className="itemgrid">
-                <div className="invitem"><div class = 'sprite' id="woodensword"></div></div>   
-                <div className="invitem"><div class = 'sprite' id="steelsword"></div></div>   
+                <div className="invitem"><div class = 'sprite' id="woodensword" draggable="true"></div></div>   
+                <div className="invitem"><div class = 'sprite' id="steelsword" draggable="true"></div></div>   
                 <div className="invitem"></div>   
                 <div className="invitem"></div>   
                 <div className="invitem"></div>   
